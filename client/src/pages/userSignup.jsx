2cc59225e13dd9636da2f6bee4831dde7ad38175
mahe1999mh <mahe1999mh@gmail.com>
2024-02-17 21:56:34 +0530

    added nav to singnup

--- client/src/pages/userSignup.jsx
+++ client/src/pages/userSignup.jsx
@@ -1,53 +1,53 @@
-
-import React, { useState } from 'react';
-import './SignupForm.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./SignupForm.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignupForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone_number: '',
-    password: ''
+    name: "",
+    email: "",
+    phone_number: "",
+    password: "",
   });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch('http://localhost:3000/signup', {
-      method: 'POST',
+    fetch("http://localhost:3000/signup", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-
-    })
-    .then(data => {
-      console.log(data); 
-    
-      setFormData({
-        name: '',
-        email: '',
-        phone_number: '',
-        password: ''
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        alert(data?.message);
+        navigate("/user");
+        setFormData({
+          name: "",
+          email: "",
+          phone_number: "",
+          password: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Error signing up:", error.message);
       });
-    })
-    .catch(error => {
-      console.error('Error signing up:', error.message);
-    });
   };
 
   return (
@@ -56,23 +56,53 @@ const SignupForm = () => {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="phone_number">Phone Number:</label>
-          <input type="tel" id="phone_number" name="phone_number" value={formData.phone_number} onChange={handleChange} required />
+          <input
+            type="tel"
+            id="phone_number"
+            name="phone_number"
+            value={formData.phone_number}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
         </div>
         <button type="submit">Sign Up</button>
       </form>
-      <h6><Link to="/user">user login</Link></h6>
+      <h6>
+        <Link to="/user">user login</Link>
+      </h6>
     </div>
   );
 };
