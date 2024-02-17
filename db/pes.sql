5854eb753210bcfbb0a054a2253610b0ce6dd0a8
mahe1999mh <mahe1999mh@gmail.com>
2024-02-17 17:29:34 +0530

    added db

--- db/pes.sql
+++ db/pes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2024 at 05:19 PM
+-- Generation Time: Feb 17, 2024 at 12:58 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `pes`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `auditorium` varchar(50) NOT NULL,
+  `start_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_date` date NOT NULL,
+  `end_time` time NOT NULL,
+  `is_pending` int(11) NOT NULL DEFAULT 1,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `auditorium`, `start_date`, `start_time`, `end_date`, `end_time`, `is_pending`, `user_id`) VALUES
+(1, '1', '2024-02-20', '09:00:00', '2024-02-20', '11:00:00', 1, 13),
+(3, '1', '2024-02-21', '09:00:00', '2024-02-22', '11:00:00', 2, 13),
+(4, '1', '2024-02-16', '22:49:00', '2024-02-17', '00:49:00', 1, 12),
+(5, '4', '2024-02-17', '15:27:00', '2024-02-19', '15:23:00', 2, 12),
+(6, '5', '2024-02-22', '15:29:00', '2024-02-21', '14:28:00', 1, 12),
+(7, '4', '2024-02-17', '12:59:00', '2024-02-21', '15:00:00', 1, 13),
+(8, '8', '2024-02-24', '17:42:00', '2024-02-29', '18:42:00', 1, 13);
 
 -- --------------------------------------------------------
 
@@ -52,6 +82,13 @@ INSERT INTO `users` (`id`, `name`, `email`, `phone_number`, `created_at`, `updat
 --
 
 --
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -64,10 +101,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
